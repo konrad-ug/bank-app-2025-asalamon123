3fcbedd72994bb2f853048755dc585db1922c5ff
Adam Salamon <asalamon@INF.UG.EDU.PL>
2025-10-21 15:37:52 +0200

    zad4 koniec

--- src/account.py
+++ src/account.py
@@ -1,9 +1,15 @@
 class Account:
-    def __init__(self, first_name, last_name, pesel):
+    def __init__(self, first_name, last_name, pesel, promo_code):
         self.first_name = first_name
         self.last_name = last_name
-        self.balance = 0
+        if promo_code and promo_code.startswith("PROMO_"):
+            self.balance = 50
+        else: 
+            self.balance = 0
+
         if len(pesel) == 11:
             self.pesel = pesel  
         else: 
             self.pesel = "Invalid"
+
+        self.promo_code = promo_code
--- tests/unit/test_account_create.py
+++ tests/unit/test_account_create.py
@@ -3,22 +3,32 @@ from src.account import Account
 
 class TestAccount:
     def test_account_creation(self):
-        account = Account("John", "Doe", "12345678912")
+        account = Account("John", "Doe", "12345678912", None)
         assert account.first_name == "John"
         assert account.last_name == "Doe"
         assert account.balance == 0
         assert account.pesel == "12345678912"
 
     def test_account_creation_short_pesel(self):
-        account = Account("John", "Doe", "123")
+        account = Account("John", "Doe", "123", None)
         assert account.first_name == "John"
         assert account.last_name == "Doe"
         assert account.balance == 0
         assert account.pesel == "Invalid"
 
     def test_account_creation_long_pesel(self): 
-        account = Account("John", "Doe", "123456789123456789")
+        account = Account("John", "Doe", "123456789123456789", None)
         assert account.first_name == "John"
         assert account.last_name == "Doe"
         assert account.balance == 0
         assert account.pesel == "Invalid"
+
+    def test_account_creation_invalid_code(self):
+        account = Account("John", "Doe", "123456789123456789", "wrong_code")
+        assert account.promo_code == "wrong_code"
+        assert account.balance == 0
+
+    def test_account_creation_valid_code(self):
+        account = Account("John", "Doe", "123456789123456789", "PROMO_ABC")
+        assert account.promo_code == "PROMO_ABC"
+        assert account.balance == 50
