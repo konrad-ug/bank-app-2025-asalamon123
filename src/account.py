3921329ea114bb914033073ed963f448e4a80479
Adam Salamon <asalamon@INF.UG.EDU.PL>
2025-10-28 14:55:45 +0100

    zad5 koniec

--- src/account.py
+++ src/account.py
@@ -2,10 +2,7 @@ class Account:
     def __init__(self, first_name, last_name, pesel, promo_code):
         self.first_name = first_name
         self.last_name = last_name
-        if promo_code and promo_code.startswith("PROMO_"):
-            self.balance = 50
-        else: 
-            self.balance = 0
+
 
         if len(pesel) == 11:
             self.pesel = pesel  
@@ -13,3 +10,44 @@ class Account:
             self.pesel = "Invalid"
 
         self.promo_code = promo_code
+
+        if self.can_get_promo():
+            self.balance = 50
+        else:
+            self.balance = 0
+
+
+    def extract_year_from_pesel(self):
+        if self.pesel == "Invalid":
+            return None
+        try:
+            yy = int(self.pesel[0:2])
+            mm = int(self.pesel[2:4])
+
+        except ValueError:
+            return None
+
+        if 1 <= mm <= 12:
+            century = 1900
+        elif 21 <= mm <= 32:
+            century = 2000
+        elif 41 <= mm <= 52:
+            century = 2100
+        elif 61 <= mm <= 72:
+            century = 2200
+        elif 81 <= mm <= 92:
+            century = 1800
+        else:
+            return None
+
+        return century + yy
+
+    def can_get_promo(self):
+        if not (self.promo_code and self.promo_code.startswith("PROMO_")):
+            return False
+
+        birth_year = self.extract_year_from_pesel()
+        if birth_year is None:
+            return False
+
+        return birth_year > 1960
--- tests/unit/test_account_create.py
+++ tests/unit/test_account_create.py
@@ -29,6 +29,34 @@ class TestAccount:
         assert account.balance == 0
 
     def test_account_creation_valid_code(self):
-        account = Account("John", "Doe", "123456789123456789", "PROMO_ABC")
+        account = Account("John", "Doe", "65010112345", "PROMO_ABC")  
         assert account.promo_code == "PROMO_ABC"
         assert account.balance == 50
+
+    def test_promo_born_after_1960(self):
+        # 1965-01-01
+        account = Account("Jan", "Kowalski", "65010112345", "PROMO_ABC")
+        assert account.balance == 50
+    
+    def test_no_promo_born_in_1960(self):
+        # 1960-01-01
+        account = Account("Ewa", "Nowak", "60010112345", "PROMO_ABC")
+        assert account.balance == 0
+    
+    def test_no_promo_for_born_before_1960(self):
+        # 1955-01-01 
+        account = Account("Adam", "Nowak", "55010112345", "PROMO_ABC")
+        assert account.balance == 0
+
+    def test_promo_for_born_after_2000(self):
+        # 2005-01-01 
+        account = Account("Ola", "Zielińska", "05210112345", "PROMO_ABC")
+        assert account.balance == 50
+
+    def test_no_promo_if_pesel_invalid(self):
+        account = Account("Piotr", "Nowak", "123", "PROMO_ABC")
+        assert account.balance == 0
+    
+    def test_no_promo_if_code_not_start_with_PROMO(self):
+        account = Account("Kasia", "Nowak", "65010112345", "DISCOUNT_50")
+        assert account.balance == 0
